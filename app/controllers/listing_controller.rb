b858946827762d1b2a34a861a3749f1c008c2f64
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2016-11-14 19:26:12 +0100

    user is able to create post, no validation as of yet

--- app/controllers/listing_controller.rb
+++ app/controllers/listing_controller.rb
@@ -5,8 +5,22 @@ class ListingController < ApplicationController
     @listings = Listing.all
   end
 
-  def create
+  def new
     @listing = Listing.new
+  end
+
+  def create
+    new_listing = Listing.new
+    new_listing.title =  params[:listing][:title]
+    new_listing.description = params[:listing][:description]
+    new_listing.user = current_user
+
+    if new_listing.save
+      redirect_to listing_index_path, notice: 'Annonsen din er publisert'
+    else
+      @listing = new_listing
+      redirect_to new_listing, notice: 'Det var en feil med annonsen din'
+    end
   end
 
   def delete
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: 'listing#index'
+  root to: 'static_page#about'
+
+  resources :listing
 end
