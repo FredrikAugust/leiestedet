bb73fa9b01af1f45842c0b3a9cb097490545af51
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2016-11-19 14:31:54 +0100

    :sparkles: you can now delete listings

--- app/controllers/listing_controller.rb
+++ app/controllers/listing_controller.rb
@@ -1,5 +1,5 @@
 class ListingController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :details]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @listings = Listing.all
@@ -17,16 +17,26 @@ class ListingController < ApplicationController
       redirect_to listing_index_path, notice: 'Annonsen din er publisert'
     else
       @listing = new_listing
-      redirect_to new_listing, notice: 'Det var en feil med annonsen din'
+      redirect_to new_listing, error: 'Det var en feil med annonsen din'
     end
   end
 
-  def delete
+  def destroy
+    listing = Listing.find(params['id'])
+
+    if listing.user == current_user
+      listing.delete
+      return redirect_to listing_index_path, notice: 'Annonsen ble slettet'
+    else
+      return redirect_to listing_path(listing),
+        error: 'Du har ikke rettigheter til aa slette denne annonsen'
+    end
   end
 
   def update
   end
 
-  def details
+  def show
+    @listing = Listing.find(params['id'])
   end
 end
