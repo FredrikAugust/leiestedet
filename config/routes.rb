4e4eea694c383c716134be13733ddba7697af2e0
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2016-12-02 15:32:33 +0100

    user interface cleanup (credit to Ask Fenn)
    TODO: fix about page from logged in user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
-
-  root to: 'static_page#about'
-
   resources :listing
+
+  authenticated do
+    root to: 'listing#index', as: :authenticated_root
+  end
+
+  root to: 'static_page#index'
+
+  #static_page
+  get 'about', to: 'static_page#index'
 end
