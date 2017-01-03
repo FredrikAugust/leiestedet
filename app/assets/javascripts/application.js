f3bf9fea664ce4da66aeff65fb6e32c7e0afd539
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2017-01-03 14:18:17 +0100

    ok, remove turbolinks, google maps doesn't work at all with this :(

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require materialize
 //= require static_page
 //= require listing
--- app/assets/javascripts/listing.js
+++ app/assets/javascripts/listing.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load', function () {
+$(document).on('load', function () {
   $('.button-collapse').sideNav();
   $('.dropdown-button').dropdown();
 });
