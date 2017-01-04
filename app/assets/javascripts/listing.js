9269e635a570a2b47ee861664e64508246c166d7
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2017-01-04 10:15:14 +0100

    added method to view images on the listing#show page

--- app/assets/javascripts/listing.js
+++ app/assets/javascripts/listing.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
   $('.button-collapse').sideNav();
   $('.dropdown-button').dropdown();
+  $('.modal').modal();
+
+  $('.listingimage').on('click', function () {
+    $('.modal-content').html($(this).clone());
+    $('.modal').modal('open');
+  });
 });
