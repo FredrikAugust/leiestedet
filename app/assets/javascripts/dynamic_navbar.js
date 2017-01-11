af4cf2a75434432c4dc1569fcc2c59b320a0b761
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2017-01-11 12:41:21 +0100

    add telephone to edit page, change shadows in dynamic nav

--- app/assets/javascripts/dynamic_navbar.js
+++ app/assets/javascripts/dynamic_navbar.js
@@ -2,8 +2,10 @@ $(document).ready(function () {
   $(window).scroll(function (e) {
     if ($(window).scrollTop() > 0) {
       $('.navbar-fixed nav').css('background', '#607d8b');
+      $('nav *').css('text-shadow', 'none');
     } else {
       $('.navbar-fixed nav').css('background', 'transparent');
+      $('nav *').css('text-shadow', '1px 1px black');
     }
   });
 });
