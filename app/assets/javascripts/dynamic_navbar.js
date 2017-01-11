1bf1335be6ee6a1070574e675d38723551b4ffc7
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2017-01-11 12:59:33 +0100

    more fluent design on front-page

--- app/assets/javascripts/dynamic_navbar.js
+++ app/assets/javascripts/dynamic_navbar.js
@@ -1,11 +1,16 @@
 $(document).ready(function () {
-  $(window).scroll(function (e) {
+  var h = $('.full-page-jumbotron').height();
+  $(window).on('wheel', function (e) {
     if ($(window).scrollTop() > 0) {
       $('.navbar-fixed nav').css('background', '#607d8b');
       $('nav *').css('text-shadow', 'none');
+      $('.full-page-jumbotron *').slideUp();
+      $('.full-page-jumbotron').height(h/2);
     } else {
       $('.navbar-fixed nav').css('background', 'transparent');
       $('nav *').css('text-shadow', '1px 1px black');
+      $('.full-page-jumbotron *').fadeIn();
+      $('.full-page-jumbotron').height(h);
     }
   });
 });
