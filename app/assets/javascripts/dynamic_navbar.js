96eaec61586e9e1f7fd3f9eabec90be42dbb2895
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2017-01-19 18:39:34 +0100

    tweaked colors and remove all shadow

--- app/assets/javascripts/dynamic_navbar.js
+++ app/assets/javascripts/dynamic_navbar.js
@@ -3,11 +3,9 @@ $(document).ready(function () {
   $(window).on('scroll', function (e) {
     if ($(window).scrollTop() > 0) {
       $('.navbar-fixed nav').css('background', 'rgba(21, 21, 21, 0.9)');
-      $('.navbar-fixed nav *').css('text-shadow', 'none');
       $('.full-page-jumbotron').height(h/2);
     } else {
       $('.navbar-fixed nav').css('background', 'transparent');
-      $('.navbar-fixed nav *').css('text-shadow', '1px 1px black');
       $('.full-page-jumbotron').height(h);
     }
   });
