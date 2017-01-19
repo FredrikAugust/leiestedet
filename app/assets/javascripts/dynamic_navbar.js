104f3ec4c20dcee9b3ea32699ba440f69ee2e053
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2017-01-19 10:08:31 +0100

    fixed #7, don't add shadow to the navbar unless it's the fixed navbar on the about us page

--- app/assets/javascripts/dynamic_navbar.js
+++ app/assets/javascripts/dynamic_navbar.js
@@ -3,12 +3,12 @@ $(document).ready(function () {
   $(window).on('wheel', function (e) {
     if ($(window).scrollTop() > 0) {
       $('.navbar-fixed nav').css('background', '#607d8b');
-      $('nav *').css('text-shadow', 'none');
+      $('.navbar-fixed nav *').css('text-shadow', 'none');
       $('.full-page-jumbotron *').slideUp();
       $('.full-page-jumbotron').height(h/2);
     } else {
       $('.navbar-fixed nav').css('background', 'transparent');
-      $('nav *').css('text-shadow', '1px 1px black');
+      $('.navbar-fixed nav *').css('text-shadow', '1px 1px black');
       $('.full-page-jumbotron *').fadeIn();
       $('.full-page-jumbotron').height(h);
     }
