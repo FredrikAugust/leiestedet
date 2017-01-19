805cc9020fe41ec429437108af272b853d4bb818
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2017-01-19 18:06:04 +0100

    make navbar b&w

--- app/assets/javascripts/dynamic_navbar.js
+++ app/assets/javascripts/dynamic_navbar.js
@@ -2,14 +2,12 @@ $(document).ready(function () {
   var h = $('.full-page-jumbotron').height();
   $(window).on('scroll', function (e) {
     if ($(window).scrollTop() > 0) {
-      $('.navbar-fixed nav').css('background', '#607d8b');
+      $('.navbar-fixed nav').css('background', 'rgba(21, 21, 21, 0.9)');
       $('.navbar-fixed nav *').css('text-shadow', 'none');
-      $('.full-page-jumbotron *').slideUp();
       $('.full-page-jumbotron').height(h/2);
     } else {
       $('.navbar-fixed nav').css('background', 'transparent');
       $('.navbar-fixed nav *').css('text-shadow', '1px 1px black');
-      $('.full-page-jumbotron *').fadeIn();
       $('.full-page-jumbotron').height(h);
     }
   });
