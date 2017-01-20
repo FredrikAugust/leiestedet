f152ab02b6fb547ee583d9efd2db5aa398c458d1
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2017-01-20 15:08:45 +0100

    touch up on front-page animation and search input styling

--- app/assets/javascripts/dynamic_navbar.js
+++ app/assets/javascripts/dynamic_navbar.js
@@ -3,10 +3,12 @@ $(document).ready(function () {
   $(window).on('scroll', function (e) {
     if ($(window).scrollTop() > 0) {
       $('.navbar-fixed nav').css('background', 'rgba(21, 21, 21, 0.9)');
-      $('.full-page-jumbotron').height(h/2);
+      $('.full-page-jumbotron').height(h*0.75);
+      $('.full-page-jumbotron .content').fadeOut();
     } else {
       $('.navbar-fixed nav').css('background', 'transparent');
       $('.full-page-jumbotron').height(h);
+      $('.full-page-jumbotron .content').fadeIn();
     }
   });
 });
