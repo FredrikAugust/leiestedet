61dfdfa68526485ee24768a4d3dd366510df73a1
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2017-01-30 16:15:01 +0100

    escape error messages @ listing@new/edit

--- app/helpers/listing_helper.rb
+++ app/helpers/listing_helper.rb
@@ -1,11 +1,2 @@
 module ListingHelper
-  def concat_errors(errors)
-    formatted_errors = []
-    errors.each do |key, value|
-      value.each do |message|
-        formatted_errors << "<span class='error'>#{key} #{message}</span>"
-      end
-    end
-    formatted_errors.join "\n"
-  end
 end
