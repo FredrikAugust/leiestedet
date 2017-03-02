b740f92280a87035d5415bc99d8035c84a2a18bf
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2017-03-02 14:23:20 +0100

    delete empty static_page.js and inc. trans. in dynamic navbar

--- app/assets/javascripts/dynamic_navbar.js
+++ app/assets/javascripts/dynamic_navbar.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   var h = $('.full-page-jumbotron').height();
   $(window).on('scroll', function (e) {
     if ($(window).scrollTop() > 0) {
-      $('.navbar-fixed nav').css('background', 'rgba(21, 21, 21, 0.9)');
+      $('.navbar-fixed nav').css('background', 'rgba(21, 21, 21, 0.7)');
       $('.full-page-jumbotron').height(h*0.75);
       $('.full-page-jumbotron .content').fadeOut();
     } else {
